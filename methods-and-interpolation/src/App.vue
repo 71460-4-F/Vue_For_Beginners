e8b7102d0f6ee7ff6e562c7d60cbc1bd04db663f
Tiago A. Fontenele <contatotiago.si@gmail.com>
2022-08-02 18:45:37 -0300

    example methods-and-interpolation

--- methods-and-interpolation/src/App.vue
+++ methods-and-interpolation/src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/views/Home.vue';
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Home/>
+      <HelloWorld msg="You did it!!!" />
     </div>
   </header>
 
---
+++ methods-and-interpolation/src/components/views/Home.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }} {{youName()}}!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello"
+    }
+  },
+
+  methods:{
+    youName(){
+        return "Tiago";
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
